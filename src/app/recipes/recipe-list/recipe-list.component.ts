2a1fcde596b770b89c7121b053453f28c2f0dc42
Tanveer Singh <tanveer.singh926@gmail.com>
2018-11-30 00:43:17 +0800

    added recipes list structure and binded with controller

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Butter Chicken',
+      'A buttery dish created with chicken',
+      'https://40aprons.com/wp-content/uploads/2018/01/whole30-indian-butter-chicken-1-2-500x500.jpg'
+    )
+  ];
 
   constructor() { }
 
