70dc18626350818f870d5920ae6975a56445a13e
Tanveer Singh <tanveer.singh926@gmail.com>
2018-11-30 01:06:19 +0800

    styled recipe detail and shopping list components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -12,6 +12,11 @@ export class RecipeListComponent implements OnInit {
       'Butter Chicken',
       'A buttery dish created with chicken',
       'https://40aprons.com/wp-content/uploads/2018/01/whole30-indian-butter-chicken-1-2-500x500.jpg'
+    ),
+    new Recipe(
+      'Butter Chicken',
+      'A buttery dish created with chicken',
+      'https://40aprons.com/wp-content/uploads/2018/01/whole30-indian-butter-chicken-1-2-500x500.jpg'
     )
   ];
 
